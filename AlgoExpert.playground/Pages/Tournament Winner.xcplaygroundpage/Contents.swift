3f106a411c480df31d7c65ac1b85210d706ad39a
Rakesh Patole <patolerakesh@gmail.com>
2021-02-18 18:16:43 +0530

    added playground for tournament winner

---
+++ AlgoExpert.playground/Pages/Tournament Winner.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func tournamentWinner(_ competitions: [[String]], _ results: [Int]) -> String {
+    var teamPoints : [String:Int] = [String:Int]()
+    var tournamentWinningTeam : String = ""
+    for (index, competiton) in competitions.enumerated() {
+        let result = results[index]
+        var matchWiningTeam = competiton[abs(result-1)]
+        teamPoints[matchWiningTeam] = (teamPoints[matchWiningTeam] ?? 0) + 3
+        if( (teamPoints[matchWiningTeam] ?? 0) > (teamPoints[tournamentWinningTeam] ?? 0)) {
+            tournamentWinningTeam = matchWiningTeam
+        }
+    }
+    return tournamentWinningTeam
+}
+
+// [Home Team, Away Team]
+// in results 0 means away tem won, 1 mean home team won the match
+let competitions = [["HTML", "C#"], ["C#", "Python"], ["Python", "HTML"]];
+let results = [0, 0, 1]
+tournamentWinner( competitions, results)
